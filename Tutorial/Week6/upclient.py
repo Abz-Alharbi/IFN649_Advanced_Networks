7dd66438212f76e64badd96d3b3324096611f1ff
Abdulaziz <alharbiabz@gmail.com>
2024-08-30 08:44:46 +1000

    Week6_test3

--- Tutorial/Week6/upclient.py
+++ Tutorial/Week6/upclient.py
@@ -6,14 +6,17 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     message = msg.payload.decode()
+    ser = serial.Serial("/dev/rfcomm1", 9600)
     if message == "LED_ON":
         print("The LED is ON")
+        ser.write(str.encode('LED_ON'))
     elif message == "LED_OFF":
         print("The LED is OFF")
+        ser.write(str.encode('LED_OFF'))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect("13.239.137.36", 1883, 60)
 client.loop_forever()
