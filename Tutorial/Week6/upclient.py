9116eb0671640959ac26dc78595312f857d1620f
Abdulaziz <alharbiabz@gmail.com>
2024-08-29 12:11:27 +1000

    Week6_Test1

---
+++ Tutorial/Week6/upclient.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT")
+    client.subscribe("ifn649")
+
+def on_message(client, userdata, msg):
+    message = msg.payload.decode()
+    if message == "LED_ON":
+        print("The LED is ON")
+    elif message == "LED_OFF":
+        print("The LED is OFF")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("13.239.137.36", 1883, 60)
+client.loop_forever()
